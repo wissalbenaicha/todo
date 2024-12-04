fead679bb6cace055b1bbe9f5f162c17c2907042
ghofrane <mohammed.samabd@gmail.com>
2024-12-04 15:40:54 +0100

    add gho.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Gho from './components/gho'; // Importation de ton composant
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        hello imene
+        <h1>Hello Imene</h1>
+        <Gho /> {/* Affichage du composant Gho */}
       </header>
     </div>
   );
 }
 
 export default App;
+
---
+++ frontend/src/components/gho.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Gho() {
+  return (
+    <div>
+      <h1>Hello from Gho Component!</h1>
+    </div>
+  );
+}
+
+export default Gho;
