d7ceff68ed5c2a7946efb8d63e4eabb2075a02a1
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-04 16:30:54 +0100

    wissal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import wissal from "./components/wissal";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello imene
-      </header>
+      <header className="App-header">hello imene</header>
+
+      <wissal />
     </div>
   );
 }
---
+++ frontend/src/components/wissal.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function wissal() {
+  return (
+    <div className="features">
+      <div className="container">
+        <div className="title">
+          <h1>Simplify Your Daily Planning</h1>
+        </div>
+        <div className="feat">
+          <h3>Task Management</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            magni sint ratione quo! Neque quaerat eligendi sit quasi sunt.
+          </p>
+        </div>
+        <div className="feat">
+          <h3>User Accounts</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            magni sint ratione quo! Neque quaerat eligendi sit quasi sunt.
+          </p>
+        </div>
+        <div className="feat">
+          <h3>Notifications and Deadlines</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            magni sint ratione quo! Neque quaerat eligendi sit quasi sunt.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default wissal;
