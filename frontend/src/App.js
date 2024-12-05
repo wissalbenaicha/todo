a75566a01ed851d963b20bd633c699e6dcf9e4a0
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-05 19:24:01 +0100

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,23 +6,6 @@ function App() {
   return (
     <div className="App">
       <Wissal /> {/* Intégration de la nouvelle page */}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        hello imene
-      </header>
-      <header className="App-header">hello imene</header>
-      <wissal />
     </div>
   );
 }
