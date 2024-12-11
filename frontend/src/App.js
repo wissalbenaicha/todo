9fda7cb2d6446ff68288c36df4e855d5444c1908
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-11 22:18:57 +0100

    wissal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,8 @@ import "./App.css";
 import Wissal from "./components/Wissal";
 import Stay from "./components/Stay";
 import Seamless from "./components/Seamless";
-
 import logo from "./logo.svg";
+
 function App() {
   return (
     <div className="App">
