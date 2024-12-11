cb245676098def43434042581b2d7527663bd206
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-11 16:22:33 +0100

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Wissal from "./components/Wissal";
+import Stay from "./components/Stay";
+import Seamless from "./components/Seamless";
+
 import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
       <Wissal /> {/* Intégration de la nouvelle page */}
+      <Stay />
+      <Seamless />
     </div>
   );
 }
---
+++ frontend/src/components/Stay.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Stay.css";
+
+function Stay() {
+  return (
+    <div className="Stay">
+      <div className="container">
+        <div className="text">
+          <h1>Stay Connected and Organized,Anytime,Anywhere</h1>
+          <p>
+            In the office, at home, or on the go, our to-do list website helps
+            you stay connected, organized, and productive by managing all your
+            tasks in one place, no matter where you are.
+          </p>
+          <a href="#">Learn more →</a>
+        </div>
+
+        {/* Section images */}
+        <div className="images">
+          <img
+            src="https://www.saeiitbhu.in/images/project/projects1.png"
+            alt=""
+          />
+          <img
+            src="https://www.saeiitbhu.in/images/project/projects2.png"
+            alt=""
+          />
+          <img
+            src="https://www.saeiitbhu.in/images/project/projects3.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Stay;
