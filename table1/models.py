4d0e2b6785141ecd4d64d5784ea9b6678ddd87b7
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-20 12:35:55 +0100

    wissal10

--- table1/models.py
+++ table1/models.py
@@ -2,6 +2,8 @@
 # Create your models here.
 from django.db import models
 
+from accounts.models import User  # Importer le modèle User
+
 class Table1(models.Model):
     PRIORITY_CHOICES = [
         ('High', 'High'),
@@ -20,6 +22,7 @@ class Table1(models.Model):
     date_echeance = models.DateField()
     priorite = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
     etat = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='taches', null=True, blank=True)
 
     def __str__(self):
         return self.nom_tache
--- table1/serializers.py
+++ table1/serializers.py
@@ -4,4 +4,4 @@ from .models import Table1
 class Table1Serializer(serializers.ModelSerializer):
     class Meta:
         model = Table1
-        fields = '__all__'
+        fields = ['id', 'nom_tache', 'date_creation', 'date_echeance', 'priorite', 'etat', 'id_user']
