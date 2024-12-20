4bf3147e99e88583ffab5445aca7a8c019a06930
imene <imene.belmadoui@outlook.com>
2024-12-20 12:57:35 +0100

    imene12

--- table1/admin.py
+++ table1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Table1, Productivity, Progress
 
-# Register your models here.
+admin.site.register(Table1)
+admin.site.register(Productivity)
+admin.site.register(Progress)
--- table1/models.py
+++ table1/models.py
@@ -1,6 +1,6 @@
-
-# Create your models here.
 from django.db import models
+from django.contrib.auth.models import User  # Pour associer les données à un utilisateur
+from accounts.models import User  # Importez le modèle User depuis l'application accounts
 
 class Table1(models.Model):
     PRIORITY_CHOICES = [
@@ -20,6 +20,35 @@ class Table1(models.Model):
     date_echeance = models.DateField()
     priorite = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
     etat = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)  # Relation vers Category
+    iduser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')  # Référence au modèle User dans l'application accounts
+
 
     def __str__(self):
         return self.nom_tache
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Productivity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)  # Utilise un ForeignKey ici
+    date = models.DateField()
+    hours_spent = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'category', 'date')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.category.name} - {self.date} - {self.hours_spent}h"
+
+class Progress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Associer à un utilisateur
+    timeframe = models.CharField(max_length=10)  # "week", "month", "year"
+    value = models.FloatField()
+
+    def __str__(self):
+        return f"Progress ({self.timeframe}): {self.value} for {self.user.username}"
