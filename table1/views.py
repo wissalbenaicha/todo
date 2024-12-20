7d007cdefbcde9afefffe0a880022f9f28b62cf4
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-20 17:08:23 +0100

    wissal10

--- table1/views.py
+++ table1/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
 from .models import Table1
 from .serializers import Table1Serializer
+from rest_framework.permissions import IsAuthenticated
 
 class Table1ViewSet(viewsets.ModelViewSet):
     queryset = Table1.objects.all()
     serializer_class = Table1Serializer
+    permission_classes = [IsAuthenticated]
+
 
     def perform_create(self, serializer):
         user = self.request.user  # Utilisateur connecté
--- todo/settings.py
+++ todo/settings.py
@@ -52,6 +52,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Par défaut, nécessite l'authentification
+    ],
 }
 
 # Configuration JWT (Tokens)
