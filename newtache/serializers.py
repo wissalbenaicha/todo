918849bb466e90119557afc709099ef443c51aaf
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-21 17:02:55 +0100

    wissal11

--- newtache/serializers.py
+++ newtache/serializers.py
@@ -1,8 +1,14 @@
-
 from rest_framework import serializers
-from newtache.models import TaskEntry  # Import du modèle TaskEntry
+from .models import TaskCategory, TaskEntry  # Import des modèles nécessaires
 
+# Serializer pour TaskCategory
+class TaskCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskCategory
+        fields = ['id', 'name']  # Inclut les champs que vous souhaitez exposer dans l'API
+
+# Serializer pour TaskEntry
 class TaskEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = TaskEntry
-        fields = ['id', 'nom_tache', 'date_creation', 'date_echeance', 'priorite', 'etat', 'category', 'id_user']
+        fields = ['id', 'nom_tache', 'date_creation', 'date_echeance', 'priorite', 'etat', 'category', 'id_user']  # Champs à exposer dans l'API
--- newtache/views.py
+++ newtache/views.py
@@ -1,18 +1,33 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from newtache.models import TaskEntry
-from newtache.serializers import TaskEntrySerializer
+from newtache.models import TaskEntry, TaskCategory
+from newtache.serializers import TaskEntrySerializer, TaskCategorySerializer
 
+# Vue pour la gestion des tâches
 class TaskEntryListCreateView(APIView):
     def get(self, request):
-        tasks = TaskEntry.objects.all()  # Obtenir toutes les tâches
+        # Obtenir toutes les tâches
+        tasks = TaskEntry.objects.all()  
         serializer = TaskEntrySerializer(tasks, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        # Créer une nouvelle tâche
         serializer = TaskEntrySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Vue pour la gestion des catégories de tâches
+class TaskCategoryListView(APIView):
+    def get(self, request):
+        # Récupère toutes les catégories de tâches
+        categories = TaskCategory.objects.all()
+        
+        # Sérialise les données
+        serializer = TaskCategorySerializer(categories, many=True)
+        
+        # Retourne les données sérialisées sous forme de réponse JSON
+        return Response(serializer.data, status=status.HTTP_200_OK)
