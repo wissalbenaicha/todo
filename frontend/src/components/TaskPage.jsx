9aa2cb334eb12ddf2f3b487ab34df68e4a620fee
wissalbenaicha <wissalbenaicha613@gmail.com>
2024-12-23 19:29:01 +0100

    wissal11

--- frontend/src/components/TaskPage.jsx
+++ frontend/src/components/TaskPage.jsx
@@ -15,7 +15,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
   if (token) {
-    config.headers.Authorization = Bearer ${token};
+    config.headers.Authorization = 'Bearer ${token}';
   }
   return config;
 });
